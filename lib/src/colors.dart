b31e477ba643e92fa716118337e0d8880ec218b8
azazadev <68111176+azazadev@users.noreply.github.com>
2021-12-01 19:41:03 +0100

    Update colors.dart

--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -6,6 +6,6 @@ const Color borderColor = Color(0xFFBCBBC1);
 const Color borderLightColor = Color.fromRGBO(49, 44, 51, 1);
 const Color backgroundGray = Color(0xFFEFEFF4);
 const Color groupSubtitle = Color(0xFF777777);
-const Color iosTileDarkColor = Color(0xFF232F34);
+const Color iosTileDarkColor = Color(0xFF000000);
 const Color iosPressedTileColorDark = Color.fromRGBO(44, 44, 46, 1);
 const Color iosPressedTileColorLight = Color.fromRGBO(230, 229, 235, 1);
